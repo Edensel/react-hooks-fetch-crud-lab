296dd78e11e44782ab8991fbb8685f287aefc517
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-07 18:05:49 +0300

    Completed assignment after 9hr

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = () => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions: ", error));
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE",
+      });
+      setQuestions((prevQuestions) =>
+        prevQuestions.filter((question) => question.id !== id)
+      );
+    } catch (error) {
+      console.error("Error deleting question: ", error);
+    }
+  };
+  
+  const handleCorrectIndexChange = async (id, correctIndex) => {
+    try {
+      await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ correctIndex }),
+      });
+      setQuestions((prevQuestions) =>
+        prevQuestions.map((question) =>
+          question.id === id ? { ...question, correctIndex } : question
+        )
+      );
+    } catch (error) {
+      console.error("Error updating correct index: ", error);
+    }
+  };
+  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={handleDelete}
+            onCorrectIndexChange={handleCorrectIndexChange}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
